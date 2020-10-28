872c672b074fe6a73d5efad3d3a25af2d6a2f9d4
rchabra <56935587+rchabra@users.noreply.github.com>
2020-10-28 15:50:14 -0700

    Updating dist for rejectpublicsuffixflag.

--- dist/electron-cookies.js
+++ dist/electron-cookies.js
@@ -13069,20 +13069,16 @@ const { Cookie } = tough;
 // it's possible to send these cookies to a domain someone could register after the fact, but
 // for Heap, we need a parseable URI because internally we try to determine the right level to set
 // a cookie, rather than having a known set of public suffix domains.
-const FAKE_APP_URI = 'https://yourdomain.heap/';
+const FAKE_APP_URI = 'https://' + window.location.host;  
 
 (function(document) {
-  const store = new WebStorageCookieStore(localStorage);
-
-  const cookiejar = new tough.CookieJar(store);
-  Object.defineProperty(document, "cookie", {
-    get() {
-      return cookiejar.getCookieStringSync(FAKE_APP_URI);
-    },
-    set(cookie) {
-    // loose: true lets us accept cookies with key and no value, which the
-    // original tests for this library included.
-      cookiejar.setCookieSync(Cookie.parse(cookie, {loose: true}), FAKE_APP_URI);
+const store = new WebStorageCookieStore(localStorage);
+const cookiejar = new tough.CookieJar(store);
+const cookiejar = new tough.CookieJar(store, false); //false here sets the rejectPublicSuffix flag
+Object.defineProperty(document, "cookie", {
+get() {
+return cookiejar.getCookieStringSync(FAKE_APP_URI);
+	
     }
   });
 })(document);
